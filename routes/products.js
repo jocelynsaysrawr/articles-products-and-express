1feabcd85e4c1aa0cd170b0ea5b2064db10f1d0b
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-03-22 18:32:16 -1000

    product get requests sanity checks

--- routes/products.js
+++ routes/products.js
@@ -3,14 +3,16 @@ const router = express.Router();
 
 module.exports = router;
 
-router.get("/", (req, res) => {
-  return res.render("index");
-});
-
-router.get("/products/:id", (req, res) => {
-  return res.render("product");
-});
-
-router.get("/products/:id/edit", (req, res) => {
-  return res.render("edit");
-});
+router
+  .get("/", (req, res) => {
+    return res.render("index");
+  })
+  .get("/products/new", (req, res) => {
+    return res.render("new");
+  })
+  .get("/products/:id", (req, res) => {
+    return res.render("product");
+  })
+  .get("/products/:id/edit", (req, res) => {
+    return res.render("edit");
+  });
